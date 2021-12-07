916f5520ea5082405f82e44d5a0b7bb51c99f97c
CoreyFulop <corey.fulop@gmail.com>
2021-12-07 16:28:51 +0000

    Completed the exercises in the first chapter of Zelle's "Python Programming"

---
+++ chaos.py
@@ -0,0 +1,14 @@
+# File: chaos_specified_list.py
+
+# A simple program illustrating chaotic behaviour.
+
+def main():
+    print("This program illustrates a chaotic function.")
+    x = eval(input("Enter a number between 0 and 1: "))
+    n = eval(input("How many numbers should I print? "))
+    for i in range(n):
+        x = 3.9 * x * (1 - x)
+        print(x)
+
+
+main()
